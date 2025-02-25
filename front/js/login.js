e82127c1a60a70cdac12dbbabae8488b59e5e071
Borilo537 <narutocamufladoxd@gmail.com>
2025-02-25 17:15:22 -0300

    new assets, switch animation and general fix

--- front/js/login.js
+++ front/js/login.js
@@ -1,20 +1,40 @@
-const regTrans = document.querySelector('.regTrans')
-const sideContainer = document.querySelector('.side-container')
-const sideContent = document.querySelector('.side-content')
+const sideContainer = document.querySelector(".side-container");
+const sideContent = document.querySelector(".side-content");
 
-setTimeout(()=>{
-    sideContent.style.opacity = '1'
-    },10)
-    
+// START PAGE ANIMATIONS
+setTimeout(() => {
+  sideContainer.style.filter = "saturate(1)";
+  sideContent.style.opacity = "1";
+}, 10);
 
-regTrans.addEventListener('click', () => {
-    sideContainer.style.maxWidth = '100%'
-    sideContent.style.opacity = '0'
+const regTrans = document.querySelector(".regTrans");
+const admSwitchRow = document.querySelector(".adm-switch-row");
+
+// ENTRY TYPE ANIMATIONS
+regTrans.addEventListener("click", () => {
+  admSwitchRow.style.opacity = "0";
+  sideContainer.style.maxWidth = "100%";
+  sideContainer.style.backgroundColor = "#f00a2d";
+  sideContent.style.opacity = "0";
+  sideContent.style.userSelect = "none";
+  sideContainer.style.filter = "saturate(0.2)";
+  setTimeout(() => {
+    sideContainer.style.maxWidth = "";
+    sideContainer.style.marginLeft = "auto";
     setTimeout(() => {
-        sideContainer.style.maxWidth = ''
-        sideContainer.style.marginLeft = 'auto'
-        setTimeout(() => {
-            window.location.href = 'register.html'
-        }, 500)
-    }, 500)
-})
+      window.location.href = "register.html";
+    }, 500);
+  }, 500);
+});
+
+const admSwitch = document.querySelector(".adm-switch");
+const admSwitchBall = document.querySelector(".adm-switch-ball");
+let adminMode = false;
+
+// ADMIN SWITCH ANIMATIONS
+admSwitch.addEventListener("click", () => {
+  adminMode = !adminMode;
+
+  admSwitchBall.classList.toggle("adm-switch-ball-active");
+  sideContainer.classList.toggle("side-container-admin");
+});
--- front/js/register.js
+++ front/js/register.js
@@ -1,19 +1,22 @@
-const regTrans = document.querySelector('.regTrans')
-const sideContainer = document.querySelector('.side-container')
-const sideContent = document.querySelector('.side-content')
+const regTrans = document.querySelector(".regTrans");
+const sideContainer = document.querySelector(".side-container");
+const sideContent = document.querySelector(".side-content");
 
-setTimeout(()=>{
-sideContent.style.opacity = '1'
-},10)
+setTimeout(() => {
+  sideContainer.style.filter = "saturate(1)";
+  sideContent.style.opacity = "1";
+}, 10);
 
-regTrans.addEventListener('click', () => {
-    sideContainer.style.maxWidth = '100%'
-    sideContent.style.opacity = '0'
+regTrans.addEventListener("click", () => {
+  sideContainer.style.maxWidth = "100%";
+  sideContent.style.opacity = "0";
+  sideContent.style.userSelect = "none";
+  sideContainer.style.filter = "saturate(0.2)";
+  setTimeout(() => {
+    sideContainer.style.maxWidth = "";
+    sideContainer.style.marginLeft = "";
     setTimeout(() => {
-        sideContainer.style.maxWidth = ''
-        sideContainer.style.marginLeft = ''
-        setTimeout(() => {
-            window.location.href = 'login.html'
-        }, 500)
-    }, 500)
-})
+      window.location.href = "login.html";
+    }, 500);
+  }, 500);
+});
