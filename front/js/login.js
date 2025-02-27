90e81fb47ab18f0676988f83cff5f34fc00e6a90
Borilo537 <narutocamufladoxd@gmail.com>
2025-02-27 15:53:09 -0300

    login bg img

--- front/js/login.js
+++ front/js/login.js
@@ -1,40 +1,30 @@
 const sideContainer = document.querySelector(".side-container");
 const sideContent = document.querySelector(".side-content");
+const loginBG = document.querySelector(".loginBG-img");
 
 // START PAGE ANIMATIONS
 setTimeout(() => {
   sideContainer.style.filter = "saturate(1)";
   sideContent.style.opacity = "1";
+  loginBG.style.opacity = "1";
 }, 10);
 
 const regTrans = document.querySelector(".regTrans");
-const admSwitchRow = document.querySelector(".adm-switch-row");
 
 // ENTRY TYPE ANIMATIONS
 regTrans.addEventListener("click", () => {
-  admSwitchRow.style.opacity = "0";
   sideContainer.style.maxWidth = "100%";
   sideContainer.style.backgroundColor = "#f00a2d";
   sideContent.style.opacity = "0";
   sideContent.style.userSelect = "none";
   sideContainer.style.filter = "saturate(0.2)";
+  loginBG.style.right = "-300px";
   setTimeout(() => {
     sideContainer.style.maxWidth = "";
     sideContainer.style.marginLeft = "auto";
+    loginBG.style.opacity = "0";
     setTimeout(() => {
       window.location.href = "register.html";
     }, 500);
   }, 500);
-});
-
-const admSwitch = document.querySelector(".adm-switch");
-const admSwitchBall = document.querySelector(".adm-switch-ball");
-let adminMode = false;
-
-// ADMIN SWITCH ANIMATIONS
-admSwitch.addEventListener("click", () => {
-  adminMode = !adminMode;
-
-  admSwitchBall.classList.toggle("adm-switch-ball-active");
-  sideContainer.classList.toggle("side-container-admin");
 });
